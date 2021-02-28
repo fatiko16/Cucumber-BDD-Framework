e7f2d3264039256f1dcc3b0ec89a1ecdbcfc46a9
Fatih <fatihealan@gmail.com>
2021-02-28 11:44:56 -0500

    Created BasePage class

--- src/test/java/com/fatiko/project_name/pages/BasePage.java
+++ src/test/java/com/fatiko/project_name/pages/BasePage.java
@@ -1,9 +1,13 @@
 package com.fatiko.project_name.pages;
 
-public class BasePage {
+import com.fatiko.project_name.utilities.Driver;
+import org.openqa.selenium.support.PageFactory;
 
-
+public abstract class BasePage {
+    //This class is base class of other pages, it initiliazes web elements that are defined in the page object classes,
+    //So we dont how to call PageFactory method in other page object classes that inherit from this class
+    //Also we can define some web elements that are common to all the classes in this class
     public BasePage(){
-            
+        PageFactory.initElements(Driver.getDriver(),this);
     }
 }
